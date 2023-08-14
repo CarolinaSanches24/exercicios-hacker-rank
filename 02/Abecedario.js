7af7cd6035426a62a0e83c2d5beaf88f0c697b61
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-08-14 17:23:24 -0300

    Resolvido Purificação

---
+++ 02/Abecedario.js
@@ -0,0 +1,12 @@
+function solucao(letra, palavras) {
+	
+    let quantidadePerdedores =0;
+    for (let i of palavras){
+        const letraInicial = i[0]
+        if(letraInicial!=letra){
+           quantidadePerdedores++
+           }
+    }
+    console.log(quantidadePerdedores)
+}
+
